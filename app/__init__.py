a590936607a2b4d299549d1ed51eb0e38c32dbc4
kan <erkan.demiralay@web.de>
2019-06-08 20:45:42 +0200

    migrated test users

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ https://www.fullstackpython.com/flask.html
 
 """
 import os
-
+import json
 import flask
 import flask_sqlalchemy
 import flask_praetorian
@@ -11,6 +11,7 @@ import flask_cors
 from app.api import api
 from app.db.engines import db_uri
 from app.backend import ecb_update
+from sqlalchemy.exc import IntegrityError, InvalidRequestError
 
 db = flask_sqlalchemy.SQLAlchemy()
 guard = flask_praetorian.Praetorian()
@@ -20,11 +21,10 @@ cors = flask_cors.CORS()
 class User(db.Model):
     __bind_key__ = 'application_data'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.Text, unique=True)
-    password = db.Column(db.Text)
-    roles = db.Column(db.Text)
-    is_active = db.Column(db.Boolean, default=True, server_default='true')
-
+    username = db.Column(db.Text, unique=True)  # username = db.Column(db.VARCHAR(200), unique=True)
+    password = db.Column(db.Text)  # password = db.Column(db.VARCHAR(200))
+    roles = db.Column(db.Text)  # roles = db.Column(db.VARCHAR(200))
+    is_active = db.Column(db.Boolean, default=True, server_default='true')  # is_active = db.Column(db.Boolean, default=True)
     @property
     def rolenames(self):
         try:
@@ -55,6 +55,25 @@ def assign_plugins(app):
     api.init_app(app)
 
 
+def migrate_test_users(app):
+    with open('migration/test_users.json', 'r') as f:
+        pw_dict = json.load(f)
+
+    with app.app_context():
+        db.create_all(bind='application_data')
+        for username in list(pw_dict):
+            try:
+                db.session.add(User(
+                    username=username,
+                    password=guard.encrypt_password(pw_dict[username]),
+                    roles='subscriber'
+                ))
+                db.session.commit()
+            except InvalidRequestError or IntegrityError:
+                print(f' user `{username}` already in db')
+    return
+
+
 def create_app():
     app = flask.Flask(__name__)
     app.config['SECRET_KEY'] = 'secret-key'
@@ -63,13 +82,16 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_BINDS'] = {
-        'application_data': 'sqlite:////home/kan/dev/py/ecb/ecb/application_data.db'
+        'application_data': os.getenv('RATES_APP_USER_DB_URL')
     }
     assign_plugins(app)
+    # migrate_test_users(app) # used for loadtesting
     return app
 
 
 app = create_app()
+
+
 '''
 try:
     with app.app_context():
@@ -83,6 +105,7 @@ try:
 except sqlalchemy.exc.IntegrityError:
     print('Database exists already')
 '''
+
 
 @app.route('/')
 def landing_page():
